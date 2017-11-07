b8ba4840b8dc82ac7fbe7ed43d2537451f10e1c7
Katlyn Tucker <studioquirk@yahoo.com>
2017-11-07 14:24:08 -0800

    working code for constructor function

--- app.js
+++ app.js
@@ -9,42 +9,42 @@ const breakfast = new Pic ('breakfast', 'imgs/breakfast.jpg');
 const bubblegum = new Pic ('bubblegum', 'imgs/bubblegum.jpg');
 const chair = new Pic ('chair', 'imgs/chair.jpg');
 const cthulhu = new Pic ('cthulhu', 'imgs/cthulhu.jpg');
-const dogduck = new Pic ('dog-duck', 'imgs/dog-duck.jpg');
+const dogDuck = new Pic ('dogDuck', 'imgs/dog-duck.jpg');
 const dragon = new Pic ('dragon', 'imgs/dragon.jpg');
 const pen = new Pic ('pen', 'imgs/pen.jpg');
-const petsweep = new Pic ('pet-sweep', 'imgs/pet-sweep.jpg');
+const petSweep = new Pic ('petSweep', 'imgs/pet-sweep.jpg');
 const scissors = new Pic ('scissors', 'imgs/scissors.jpg');
 const shark = new Pic ('shark', 'imgs/shark.jpg');
 const sweep = new Pic ('sweep', 'imgs/sweep.jpg');
 const tauntaun = new Pic ('tauntaun', 'imgs/tauntaun.jpg');
 const unicorn = new Pic ('unicorn', 'imgs/unicorn.jpg');
-const usbgif = new Pic ('usb', 'imgs/usb.gif');
-const watercan = new Pic ('water-can', 'imgs/water-can.jpg');
-const wineglass = new Pic ('wine-glass','imgs/wine-glass.jpg');
+const usb = new Pic ('usb', 'imgs/usb.gif');
+const waterCan = new Pic ('waterCan', 'imgs/water-can.jpg');
+const wineGlass = new Pic ('wineGlass','imgs/wine-glass.jpg');
 
-const picArray = [bag, banana, bathroom, boots, breakfast, bubblegum ,chair, cthulhu, dog-duck, dragon, pen, pet-sweep, scissors, shark, sweep, tauntaun, unicorn, usb, water-can, wine-glass];
+const picArray = [bag, banana, bathroom, boots, breakfast, bubblegum ,chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+
+function Pic (type,src){
+        this.type = type;
+        this.src = src;
+        this.picked = 0;
+    }
+
 console.log (picArray);
 
-//let myArray = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19']
+//function appendRandomPic();
+    //picGame.prototype.wasPicked = function {
+    //   this.picked +=1;
+    ////};
+   // render() {
+//    const ele = document.createElement('img');
+ //       ele.src = this.src;
+        //to do - check if it's adding a class)
+  //      return ele;
+  //  }
+//}
+//console.log(src);
 
-
-//constructor function for random photo picker//
-function Pic (type,src){
-    this.type = type ;
-    this.src = src;
-    this.picked = 0;
-}
-
-//picGame.prototype.wasPicked = function {
- //   this.picked +=1;
-////};
-
-Pic.prototype.render = function(){
-    const ele = document.createElement('img');
-    ele.src = this.src;
-    //to do - check if it's adding a class)
-    return ele;
-}
 
 //let clicks = 0;
 //let Pic;
@@ -58,15 +58,12 @@ Pic.prototype.render = function(){
  //   const clickedImage = e.target;
  //   console.log ('alrighty then');
 //}
-
-function appendrandomPic();
-
-for (let i = 0; i < 3 ; i++){
-const Game = document.getElementById('Game');
-randomPic = picArray[ Math.floor(Math.random()* 20)];
-const randomPicEle = randomPic.render();
-Game.appendChild(randomPicEle);
-}
+//for (let i = 0; i < 3 ; i++){
+//const Game = document.getElementById('Game');
+//randomPic = picArray[ Math.floor(Math.random()* 20)];
+//const randomPicEle = randomPic.render();
+//Game.appendChild(randomPicEle);
+//}
 
 
 //const main = document.querySelector('main');
